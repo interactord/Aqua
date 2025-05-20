4653ac321b277a20e2830865442f6e82fe9ff45c
Scott Moon <interactord@gmail.com>
2025-05-20 13:46:49 +0900

    reset

--- main.py
+++ main.py
@@ -1,31 +1,12 @@
-import os
-from fastapi import FastAPI, Request
-from pydantic import BaseModel
-from typing import List
-from wtpsplit import SaT
-
-os.environ["TOKENIZERS_PARALLELISM"] = "true"
-
-model_ort = SaT("sat-3l-sm", ort_providers=["CPUExecutionProvider"])
-
-def split_text(text: str) -> List[str]:
-    result = list(model_ort.split(text))
-    return result
+from fastapi import APIRouter, FastAPI
 
 app = FastAPI()
+# app.include_router(classifier.router)
 
-class TextRequest(BaseModel):
-    originText: str
+# if __name__ == "__main__":
+#     import uvicorn
+#     uvicorn.run(app, host="0.0.0.0", port=8000)
 
 @app.get("/")
-async def greetings():
-    return "Hello World!"
-
-@app.post("/split")
-async def split_text_get(request: Request):
-    body = await request.json()
-    text = body.get("originText")
-    if not text:
-        return {"error": "originText 필드가 필요합니다."}
-    result = split_text(text)
-    return {"result": result}
+async def root():
+    return {"message": "hello world"}
